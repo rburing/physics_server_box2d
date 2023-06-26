e2f916490bc01ae27c02989aaefd78e42ac3f6d2
Dragos Daian <daian.dragos@yahoo.com>
2023-06-26 16:58:11 +0200

    update readme and recording

--- record.sh
+++ record.sh
@@ -2,6 +2,6 @@ sed -i '' 's/physics_engine=.*/physics_engine="GodotPhysics2D"/' "demo/project.g
 /Applications/Godot-2.app/Contents/MacOS/Godot --path ./demo --debug-collisions --write-movie recording/GodotPhysics2D.avi
 sed -i '' 's/physics_engine=.*/physics_engine="Box2D"/' "demo/project.godot" 
 /Applications/Godot-2.app/Contents/MacOS/Godot --path ./demo --debug-collisions --write-movie recording/Box2D.avi
-ffmpeg -i demo/recording/Box2D.avi -i demo/recording/GodotPhysics2D.avi -filter_complex \
+ffmpeg -y -i demo/recording/Box2D.avi -i demo/recording/GodotPhysics2D.avi -filter_complex \
 "[0:v]setpts=PTS-STARTPTS, pad=iw*2:ih[bg]; \
-[1:v]setpts=PTS-STARTPTS[fg]; [bg][fg]overlay=w" output.gif
+[1:v]setpts=PTS-STARTPTS[fg]; [bg][fg]overlay=w" -r 10 output.gif
